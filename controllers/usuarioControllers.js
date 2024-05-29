5f99de93ec87024697c9b1603c64d273749e3ec5
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-29 17:33:13 -0600

    feat: validate create user and try and catch

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,29 +1,46 @@
-const Usuario = require('../models/usuario');
+const Usuario = require("../models/usuario");
 
 const getUsuarios = async (req, res) => {
+  const usuarios = await Usuario.find({}, "nombre email role google");
 
-    const usuarios = await Usuario.find({}, 'nombre email role google');
-
-    res.json({
-        ok: true,
-        usuarios
-    })
-}
+  res.json({
+    ok: true,
+    usuarios,
+  });
+};
 
 const crearUsuario = async (req, res) => {
-    
-    const usuario =  Usuario(req.body);
+  const { email } = req.body;
 
+  try {
+    const emailExist = Usuario.findOne({ email });
+
+    if (emailExist) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo ya existe",
+      });
+    }
+
+    const usuario = Usuario(req.body);
     await usuario.save();
 
-    res.json({ 
-        ok: true,        
-        msg: 'Usuario creado',
-        usuario
-    })
-}
+    res.json({
+      ok: true,
+      msg: "Usuario creado",
+      usuario,
+    });
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado... revisar logs",
+    });
+  }
+};
 
-module.exports = { 
-    getUsuarios,
-    crearUsuario
-}
+module.exports = {
+  getUsuarios,
+  crearUsuario,
+};
