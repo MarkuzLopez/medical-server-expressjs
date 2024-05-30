3fee78976c01115d616f34166f18c5e72d2af321
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-29 18:02:31 -0600

    feat: :sparkles: express validator and middleware for user
    create middleware for user and import in routes
    
    install express validator

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,4 +1,5 @@
 const Usuario = require("../models/usuario");
+const { validationResult } = require('express-validator');
 
 const getUsuarios = async (req, res) => {
   const usuarios = await Usuario.find({}, "nombre email role google");
@@ -11,6 +12,14 @@ const getUsuarios = async (req, res) => {
 
 const crearUsuario = async (req, res) => {
   const { email } = req.body;
+// TODO remove for new field in middleware and routes users
+//   const erros = validationResult(req);
+//   if (!erros.isEmpty()) {
+//     return res.status(400).json({
+//         ok: false,
+//         erros: erros.mapped()
+//     })
+//   }
 
   try {
     const emailExist = Usuario.findOne({ email });
@@ -30,7 +39,7 @@ const crearUsuario = async (req, res) => {
       msg: "Usuario creado",
       usuario,
     });
-    
+
   } catch (error) {
     console.error(error);
     res.status(500).json({
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,11 +1,22 @@
 /**
  * Ruta: /api/usuarios
  */
+const { check } = require('express-validator')
 const { Router } = require('express')
 const { getUsuarios, crearUsuario } = require('../controllers/usuarioControllers');
+const { validarCampos } = require('../middlewares/validarCampos');
 
 const router = Router()
 
 router.get('/',  getUsuarios );
-router.post('/create', crearUsuario )
+router.post('/create',
+//middelware
+[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'el password es obligatorio').not().isEmpty(),
+    check('email', 'el email es obligatorio').isEmail(),
+    validarCampos
+],
+
+crearUsuario )
 module.exports = router;
