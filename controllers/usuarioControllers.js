c4f74415e831b0d10557e3f9729cf0b8f0970171
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-29 18:51:51 -0600

    feat: :sparkles: update users
    validate update users and add  route

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const Usuario = require("../models/usuario");
 const bcrypt = require('bcryptjs');
 
@@ -56,7 +57,56 @@ const crearUsuario = async (req, res) => {
   }
 };
 
+const actualizarUsuario = async (req, res = response ) => { 
+
+    const uid = req.params.id;
+
+    console.log('entraaaa qui', uid);
+    try {
+
+        const existUserDB =  await Usuario.findById(uid);
+        if(!existUserDB){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Usuario no existe por ese id'
+            })
+        }
+
+        const campos = req.body;
+        if(existUserDB.email === req.body.email){
+            delete campos.email;
+        } else { 
+            const existEmail =  await Usuario.findOne({ email: req.body.email})
+
+            if(existEmail) { 
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                })
+            }
+        }
+
+        delete campos.password;
+        delete campos.google;
+
+        const usuarioActualizado  = await Usuario.findByIdAndUpdate( uid, campos, { new: true } )
+        res.status(201).json({
+            ok: true,
+            msg: 'Usuario actualizado',
+            usuarioActualizado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+}
+
 module.exports = {
   getUsuarios,
   crearUsuario,
+  actualizarUsuario
 };
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,22 +1,37 @@
 /**
  * Ruta: /api/usuarios
  */
-const { check } = require('express-validator')
-const { Router } = require('express')
-const { getUsuarios, crearUsuario } = require('../controllers/usuarioControllers');
-const { validarCampos } = require('../middlewares/validarCampos');
+const { check } = require("express-validator");
+const { Router } = require("express");
+const {
+  getUsuarios,
+  crearUsuario,
+  actualizarUsuario,
+} = require("../controllers/usuarioControllers");
+const { validarCampos } = require("../middlewares/validarCampos");
 
-const router = Router()
+const router = Router();
 
-router.get('/',  getUsuarios );
-router.post('/create',
-//middelware
-[
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('password', 'el password es obligatorio').not().isEmpty(),
-    check('email', 'el email es obligatorio').isEmail(),
-    validarCampos
-],
+router.get("/", getUsuarios);
+router.post(
+  "/create",
+  //middelware
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "el password es obligatorio").not().isEmpty(),
+    check("email", "el email es obligatorio").isEmail(),
+    validarCampos,
+  ],
 
-crearUsuario )
+  crearUsuario
+);
+
+router.put(
+  "/update/:id",
+  check("nombre", "El nombre es obligatorio").not().isEmpty(),
+  check("password", "el password es obligatorio").not().isEmpty(),
+  check("email", "el email es obligatorio").isEmail(),
+  actualizarUsuario
+);
+
 module.exports = router;
