438bad7c0787ee5c521d833bc16dbce1b1bd6d7a
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-31 18:09:14 -0600

    feat: search values of medicos, hoispitales, and users

--- controllers/busquedaController.js
+++ controllers/busquedaController.js
@@ -1,14 +1,29 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
+const Medico = require("../models/medico");
+const Hospital = require("../models/hospital");
 
-const getTodo = (req, res =  response) => {
+const getTodo = async (req, res =  response) => {
 
-    console.log(req.params, 'params');
-    const busqueda = req.params.id
+    const busqueda = req.params.id;
+    const regex = new RegExp(busqueda, 'i')// para datos insensibles(es decir que buqeu, sin se explicito)
+
+    // const usuarios =    await Usuario.find({ nombre: regex })
+    // const medicos =     await Medico.find({ nombre: regex })
+    // const hospitales =  await Hospital.find({ nombre: regex })
+
+    const [usuarios, medicos, hospitales ] = await Promise.all([
+        await Usuario.find({ nombre: regex }),
+        await Medico.find({ nombre: regex }),
+        await Hospital.find({ nombre: regex })
+    ])
 
     res.json({
         ok: true,
         msg: 'getTodo',
-        busqueda
+        usuarios,
+        medicos,
+        hospitales
     })
 } 
 
