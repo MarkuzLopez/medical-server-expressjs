99e5c8966396ea3197d4c480612826debf504667
MarkuzLopez <marco.02lopez@hotmail.com>
2024-06-04 12:17:49 -0600

    feat: search by collection DB all

--- controllers/busquedaController.js
+++ controllers/busquedaController.js
@@ -3,6 +3,41 @@ const Usuario = require('../models/usuario');
 const Medico = require("../models/medico");
 const Hospital = require("../models/hospital");
 
+const getDcoumentsCollection = async (req, res = response ) => { 
+    const tabla = req.params.tabla;
+    const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, 'i');
+
+    let data;
+
+    switch (tabla) {
+        case 'medicos':
+            data = await Medico.find({nombre: regex})
+                    .populate('hospital', 'nombre')
+                    .populate('usuario', 'nombre img');
+            break;
+            case 'hospitales':
+                data = await Hospital.find({ nombre: regex })
+                                .populate('usuario', 'nombre img')
+            break;
+            case 'usuarios':
+                data = await Usuario.find({ nombre: regex })
+            break;
+
+    
+        default:
+            return res.status(400).json({ 
+                ok: false,
+                msg: 'la tabla tiene que ser usaurios/medicos/hospitales',
+            })        
+    }
+    
+    res.json({ 
+        ok: true,
+        resultado: data
+    })
+}
+
 const getTodo = async (req, res =  response) => {
 
     const busqueda = req.params.id;
@@ -28,5 +63,6 @@ const getTodo = async (req, res =  response) => {
 } 
 
 module.exports = { 
-    getTodo
+    getTodo, 
+    getDcoumentsCollection
 }
--- routes/busquedaRoutes.js
+++ routes/busquedaRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
-const { getTodo } = require("../controllers/busquedaController");
+const { getTodo, getDcoumentsCollection } = require("../controllers/busquedaController");
 const { validarJwt } = require("../jwt/jwt");
 
 
 const router = Router();
 
 router.get('/:id', validarJwt, getTodo);
+//FIXME busqueda por coleccion especifiica
+router.get('/collection/:tabla/:busqueda', validarJwt, getDcoumentsCollection)
 
 module.exports = router;
